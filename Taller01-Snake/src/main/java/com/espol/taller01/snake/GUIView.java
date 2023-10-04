b50363ea007293e9214e3d1f97aa3d027fe47ed8
Cristhian Santacruz <crimsant@espol.edu.ec>
2023-10-04 16:25:18 -0500

    cambio de nombre del bootn startGameButton

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
@@ -17,7 +17,7 @@ import javax.swing.JPanel;
  */
 public class GUIView extends JPanel {
 	/** The "Start Game" button */
-	private final JButton letsplay;
+	private final JButton startGameButton;
 
 	/** The chooser (also called drop-down menu) with names of different games */
 	private final JComboBox gameChooser;
@@ -63,7 +63,7 @@ public class GUIView extends JPanel {
 
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
-		this.startGameButton = new JButton("Let's go!!!");
+		this.startGameButton = new JButton("Let's play!!!");
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
 
